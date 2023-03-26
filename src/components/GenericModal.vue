e2557a8ec81194a8a56ee28e3cd5a81abf5a9b1e
tsomic <keller.nils99@gmail.com>
2023-03-27 01:36:54 +0200

    add backdrop to modal

--- src/components/GenericModal.vue
+++ src/components/GenericModal.vue
@@ -1,14 +1,19 @@
 <template>
-  <div
-    v-if="modelValue"
-    class="absolute top-0 bottom-0 left-0 right-0 w-3/4 h-fit z-50 m-auto p-4 pt-8 rounded-lg bg-blue-900"
-  >
-    <font-awesome-icon
-      icon="fa-solid fa-xmark"
-      @click="emit('update:modelValue', false)"
-      class="w-6 h-6 m-2 text-xl text-white cursor-pointer absolute top-0 right-0"
+  <div v-if="modelValue">
+    <div
+      @click="close"
+      class="absolute w-full h-full top-0 left-0 z-40 bg-[#00000088]"
     />
-    <slot />
+    <div
+      class="absolute top-0 bottom-0 left-0 right-0 w-3/4 h-fit z-50 m-auto p-4 pt-8 rounded-lg bg-blue-900"
+    >
+      <font-awesome-icon
+        icon="fa-solid fa-xmark"
+        @click="close"
+        class="w-6 h-6 m-2 text-xl text-white cursor-pointer absolute top-0 right-0"
+      />
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -18,4 +23,8 @@ defineProps<{
 }>();
 
 const emit = defineEmits(["update:modelValue"]);
+
+const close = () => {
+  emit("update:modelValue", false);
+};
 </script>
