a419882fdfd09bc0bcf6885af10fc11a03dda316
tsomic <keller.nils99@gmail.com>
2023-04-29 02:03:54 +0200

    add some link logic

--- src/components/JointDiagram.vue
+++ src/components/JointDiagram.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     ref="paperDiv"
-    class="w-full h-full m-auto border-2 border-black border-solid bg-black"
+    class="w-full h-full m-auto border-2 border-black border-solid"
   />
 </template>
 
@@ -16,12 +16,19 @@ const paperDiv = ref<HTMLDivElement | null>(null);
 const { faces } = storeToRefs(useFacesStore());
 const { elements } = storeToRefs(useDiagramStore());
 const animationInterval = ref<number>();
+const graph = new joint.dia.Graph();
+const paper = ref();
 
 const switchAllFaces = () => {
   elements.value.forEach(switchFace);
 };
 
+const toolsView = new joint.dia.ToolsView({
+  tools: [new joint.linkTools.Remove(), new joint.linkTools.TargetArrowhead()],
+});
+
 // TODO fix typing
+// also: https://resources.jointjs.com/tutorials/joint/tutorials/Tsshape.html
 const switchFace = (element: joint.shapes.basic.Image | any) => {
   const images = element.prop("images");
   const currentImage = element.attr("image/xlinkHref");
@@ -29,44 +36,69 @@ const switchFace = (element: joint.shapes.basic.Image | any) => {
   element.attr("image/xlinkHref", images[currentImage === images[0] ? 1 : 0]);
 };
 
+const fillFromStore = () => {
+  clearInterval(animationInterval.value);
+
+  graph.clear();
+
+  elements.value = [];
+
+  faces.value.forEach((face, index) => {
+    elements.value.push(
+      addElement(index * 40, Math.random() * 400, face.name, face.images)
+    );
+  });
+
+  animationInterval.value = setInterval(switchAllFaces, 1e3);
+
+  const link = new joint.shapes.standard.Link();
+
+  link.source(elements.value[0]);
+  link.target(elements.value[1]);
+  link.addTo(graph);
+};
+
 watch(
   () => faces.value.length,
   () => {
-    clearInterval(animationInterval.value);
-
-    faces.value.forEach((face) => {
-      elements.value.push(addElement(100, 100, face.name, face.images));
-    });
-
-    animationInterval.value = setInterval(switchAllFaces, 1e3);
-
-    // var link = new joint.shapes.standard.Link({
-    //   attrs: {
-    //     line: {
-    //       stroke: "white",
-    //     },
-    //   },
-    // });
-    // link.source(hello);
-    // link.target(world);
-    // link.addTo(graph);
+    fillFromStore();
   }
 );
 
 onMounted(() => {
-  new joint.dia.Paper({
+  paper.value = new joint.dia.Paper({
     el: paperDiv.value,
     width: "100%",
     height: "100%",
     model: graph,
-    defaultConnectionPoint: { name: "boundary" },
-    defaultConnector: { name: "smooth" },
-    interactive: { linkMove: false },
-  });
-});
+    gridSize: 100,
+    linkPinning: false,
+    defaultLink: () => new joint.shapes.standard.Link(),
+    validateConnection: (cellViewS, magnetS, cellViewT, magnetT) => {
+      const links = graph.getLinks();
 
-let namespace = joint.shapes;
-let graph = new joint.dia.Graph({}, { cellNamespace: namespace });
+      const isUniqueConnection = links.every(
+        (existingLink) =>
+          !(
+            existingLink.source().id === cellViewS.model.id &&
+            existingLink.target().id === cellViewT.model.id
+          )
+      );
+
+      return isUniqueConnection && magnetS.id !== magnetT.id;
+    },
+  });
+
+  paper.value.on("link:mouseenter", (linkView: joint.dia.LinkView) => {
+    linkView.addTools(toolsView);
+  });
+
+  paper.value.on("blank:mouseover", () => {
+    paper.value.removeTools();
+  });
+
+  fillFromStore();
+});
 
 const addElement = (
   x: number,
@@ -88,6 +120,7 @@ const addElement = (
         width: 32,
         height: 16,
         imageRendering: "pixelated",
+        magnet: true,
       },
     },
   });
